9bb2699146cbe9c83dae3336b98363443fff82bc
thamim358 <thamimansari358@gmail.com>
2024-10-14 19:51:35 +0530

    auto deployement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',  // Ensure correct relative paths for assets
   server: {
     port: 3000 // Specify the port you want to use
   },
